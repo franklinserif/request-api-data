93182d5bc4dc210b886ad7184962bccb9686380b
franklin rodriguez <franklinserif@gmail.com>
2022-06-13 18:31:41 -0400

    complete

--- src/app.js
+++ src/app.js
@@ -15,17 +15,6 @@ async function auth(url) {
 
 app.get("/auth", async (req, res) => {
   const response = await auth(authUrl);
-  try {
-    pool.query(
-      "SELECT * FROM ficha_de_cliente",
-      function (error, results, fields) {
-        if (error) throw error;
-        //console.log("sql query result: ", results[0]);
-      }
-    );
-  } catch (error) {
-    console.log(`mysql error: ${error}`);
-  }
 
   res.redirect(response.request.res.responseUrl);
 });
@@ -45,18 +34,12 @@ app.get("/token", async (req, res) => {
   });
 
   const items = await axiosAuth.get(
-    "https://api.podio.com/item/app/25014884?limit=500"
+    "https://api.podio.com/item/app/25918616?limit=500"
   );
-  const headers = [
-    "Creado el",
-    "Empresa",
-    "Fecha de encuesta",
-    "¿Cuál es la probabilidad de que recomiendes nuestro servicio a otra empresa?",
-    "¿Qué tan satisfecho/a estas con el avance del proyecto en función de los objetivos planteados y los desafíos de tu empresa?",
-    "¿Qué tan satisfecho/a estas con el direccionamiento del proyecto por parte del consultor/a?",
-    "¿Qué tan satisfecho/a estás con el plan de trabajo de  este mes?",
-    "¿Cómo evalúas la implementación por parte de tu empresa de las estrategias sugeridas por ArchGroup este mes?",
-  ];
+
+  const tableNames = ["creado_el", "categoria"];
+
+  const headers = ["Creado el", "Categoría"];
 
   const fichacliente = [];
 
@@ -66,10 +49,10 @@ app.get("/token", async (req, res) => {
     currentItem.push({ title: "Creado el", value: item.created_on });
 
     Object.values(fields).forEach((field) => {
-      if (headers.includes(field.label)) {
+      if (headers.includes(field.label.trim())) {
         if (field.values[0].value?.text) {
           currentItem.push({
-            title: field.label,
+            title: field.label.trim(),
             value: field.values[0].value.text,
           });
         } else if (field.values[0].value?.start_date_utc) {
@@ -113,7 +96,7 @@ app.get("/token", async (req, res) => {
       } */
     });
 
-    console.table(newItem);
+    //console.table(newItem);
     fichacliente.push([...newItem]);
   });
 
@@ -134,9 +117,6 @@ app.get("/token", async (req, res) => {
       console.log(error);
     }
   });
-
-  res.send("completed");
-});
 
   res.send("completed");
 });

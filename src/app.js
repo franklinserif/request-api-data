fb10f41ddc0bd2d5695db636f329b7d47224c975
franklin rodriguez <franklinserif@gmail.com>
2022-06-13 11:25:49 -0400

    uncompleted

--- src/app.js
+++ src/app.js
@@ -34,15 +34,40 @@ app.get("/token", async (req, res) => {
   });
 
   const items = await axiosAuth.get(
-    "https://api.podio.com/item/app/24989667?limit=500"
+    "https://api.podio.com/item/app/26520248?limit=500"
   );
   const headers = [
-    "Fecha de reunión",
-    "Empresa",
-    "Consultor",
-    "Nº de reunión",
-    "Estado",
-    "Qué se hizo",
+    "cliente",
+    "Nº reuniones o demos realizados",
+    "Nº propuestas realizadas",
+    "Mes",
+    "Semana - start",
+    "Semana - end",
+    "Reuniones generadas por: Outbound",
+    "Reuniones en Argentina",
+    "Cierres en Argentina",
+    "Reuniones generadas por: Prospectin",
+    "Reuniones en Chile",
+    "Cierres en Chile",
+    "Reuniones generadas por: Redes",
+    "Reuniones en Colombia",
+    "Cierres en Colombia",
+    "Reuniones generadas por: Google Ads",
+    "Reuniones en España",
+    "Cierres en España",
+    "Reuniones generadas por: Orgánico",
+    "Reuniones en México",
+    "Cierres en México",
+    "Reuniones generadas por: Webinar /charla /similar",
+    "Reuniones en Perú",
+    "Cierres en Perú",
+    "Reuniones generadas por: partners",
+    "Reuniones en  USA",
+    "Cierres en USA",
+    "Reuniones generadas por: Referidos o contactos",
+    "Reuniones en otros mercados",
+    "Cierres en otros mercados",
+    "Nº de cierres realizados",
   ];
 
   const fichacliente = [];
@@ -55,7 +80,16 @@ app.get("/token", async (req, res) => {
       value: item.created_on,
     }); */
     Object.values(fields).forEach((field) => {
-      if (headers.includes(field.label.trim())) {
+      if (headers[3].trim() === field.label) {
+        currentItem.push({
+          title: headers[4],
+          value: field.values[0].value.start_date_utc,
+        });
+        currentItem.push({
+          title: field.label.trim(),
+          value: field.values[0].value.text,
+        });
+      } else if (headers.includes(field.label.trim())) {
         if (field.values[0].value?.text) {
           currentItem.push({
             title: field.label.trim(),
